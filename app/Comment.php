a520111b0595d79dbdb5ad9c935bb96c447f68d7
Unknown <massive.annas@gmail.com>
2019-04-28 14:02:30 +0700

    added comments table

---
+++ app/Comment.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    protected $fillable = [
+        'id_pertanyaan', 'id_user', 'komentar', 'jumlah_vote'
+    ];
+
+}
---
+++ database/migrations/2019_04_28_062705_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('id_pertanyaan')->unsigned();
+            $table->integer('id_user')->unsigned();
+            $table->text('komentar');
+            $table->integer('jumlah_vote')->unsigned();
+            $table->timestamp('waktu');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
