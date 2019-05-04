6be3fa66ee6762531fc876267ebd5404515ac2b1
Arief Prasetyo <ariefp3101@gmail.com>
2019-05-05 01:46:24 +0700

    tatag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,12 +12,11 @@
   <title>TCOVERFLOW</title>
 
   <!-- Custom fonts for this template-->
-  <link rel="stylesheet" href="vendor/fontawesome-free/css/all.min.css" type="text/css" media="all">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" media="all">
+  <link rel="stylesheet" href="{{asset('vendor/fontawesome-free/css/all.min.css')}}" type="text/css" media="all">
+  <link rel="stylesheet" href="{{asset('https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i')}}" media="all">
 
   <!-- Custom styles for this template-->
-  <link href="css/sb-admin-2.min.css" rel="stylesheet">
-  @yield('style')
+  <link href="{{asset('css/sb-admin-2.min.css')}}" rel="stylesheet">
 </head>
 
 <body id="page-top">
@@ -264,14 +263,14 @@
   </div>
 
   <!-- Bootstrap core JavaScript-->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
+  <script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="{{asset('vendor/jquery-easing/jquery.easing.min.js')}}"></script>
 
   <!-- Custom scripts for all pages-->
-  <script src="js/sb-admin-2.min.js"></script>
+  <script src="{{asset('js/sb-admin-2.min.js')}}"></script>
 
 </body>
 
--- resources/views/pertanyaan/tambah.blade.php
+++ resources/views/pertanyaan/tambah.blade.php
@@ -18,12 +18,16 @@
         		<div class="card-header border-0">
           			<h3 class="mb-0">Buat Pertanyaan</h3>
         		</div>
-    			<div class="offset-md-1 mb-4 mt-4">			
+    			<div class="ml-4 mb-4 mt-4 mr-4">			
 				<form action="{{ route('pertanyaan.store')}}" method="post" class="" enctype="multipart/form-data">
 					{{ csrf_field()}}
 					<div class="form-group has-feedback">
 						<label for="" class="text mt-1" style="font-size: 20px;">Judul</label>
 						<input type="text" class="form-control inp" name="judul" placeholder="Judul" required>
+					</div>
+					<div class="form-group has-feedback">
+						<label for="" class="text mt-1" style="font-size: 20px;">Tag</label>
+						<input type="text" class="form-control inp" name="tag" placeholder="Tag" required>
 					</div>
 					<div class="form-group has-feedback">
 						<label for="" class="tex" style="font-size: 20px;">Pertanyaan</label>
